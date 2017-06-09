8e9849ecfa30ee76233221d5dceca6393e67e8e3
Martijn Braam <martijn@brixit.nl>
2017-06-10 01:38:46 +0200

    Added uboot script to n900

---
+++ device-nokia-rx51/uboot-script.cmd
@@ -0,0 +1,14 @@
+setenv mmcnum 0
+setenv mmcpart 1
+setenv mmctype ext2
+setenv setup_omap_atag 1
+setenv bootargs init=/init.sh rw console=tty0 console=tty02 omapfb_vram=7M omapfb.mode=lcd:848x480-16 nokia-modem.pm=0
+setenv mmckernfile /uImage
+setenv mmcinitrdfile /uInitrd
+setenv mmcscriptfile
+echo Loading initramfs
+run initrdload
+echo Loading kernel
+run kernload
+echo Booting kernel
+run kerninitrdboot
