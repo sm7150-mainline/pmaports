41548617941b516fd16d1be2d85f04cea3626c39
Pablo Castellano <pablo@anche.no>
2017-06-16 22:38:11 +0200

    [postmarketos-base][hammerhead] Use weston --pixman-type
    Add a new optional variable weston_pixman_type in deviceinfo
    This is used to fix the red screen issue (close #54)

--- postmarketos-base/profile.sh
+++ postmarketos-base/profile.sh
@@ -1,5 +1,7 @@
 export DISPLAY=:0
 
+. /etc/deviceinfo
+
 if test -z "${XDG_RUNTIME_DIR}"; then
 	# https://wayland.freedesktop.org/building.html
 	export XDG_RUNTIME_DIR=/tmp/$(id -u)-runtime-dir
@@ -11,10 +13,15 @@ if test -z "${XDG_RUNTIME_DIR}"; then
 	# Weston autostart on tty1 (Autologin on tty1 is enabled in
 	# /etc/inittab by postmarketos-base post-install.hook)
 	if [ $(tty) = "/dev/tty1" ]; then
+        WESTON_OPTS="--backend=fbdev-backend.so"
+        if test -n "${deviceinfo_weston_pixman_type}"; then
+            WESTON_OPTS="${WESTON_OPTS} --pixman-type=${deviceinfo_weston_pixman_type}"
+        fi
+
 		udevadm trigger
 		udevadm settle
 		(sleep 2; postmarketos-demos) &
-		weston --backend=fbdev-backend.so >/tmp/weston.log 2>&1
+		weston ${WESTON_OPTS} >/tmp/weston.log 2>&1
 
 		# In case of failure, restart after 1s
 		sleep 1
