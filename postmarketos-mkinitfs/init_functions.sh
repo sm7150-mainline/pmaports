3ca1a2b6419ec18309988a04dd2508d89e5e5361
Pablo Castellano <pablo@anche.no>
2017-06-22 19:36:44 +0200

    Mount unencrypted root partition (close #86)
    - Do not decrypt if not necessary
    - Do not launch telnetd if partition is unencrypted
    - Basic initramfs logging to /tmp/boot.log

--- postmarketos-mkinitfs/init_functions.sh
+++ postmarketos-mkinitfs/init_functions.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # This file will be in /init_functions.sh inside the initramfs.
 
+log()
+{
+	echo "[$1] $2" >> /tmp/boot.log
+}
+
 mount_subpartitions()
 {
 	for i in /dev/mmcblk*; do
@@ -32,15 +37,22 @@ find_root_partition()
 
 unlock_root_partition()
 {
+	log "info" "unlock_root_partition()"
 	while ! [ -e /dev/mapper/root ]; do
 		partition="$(find_root_partition)"
 		if [ -z "$partition" ]; then
-			echo "Could not find cryptsetup partition."
+			echo "Could not find the root partition."
 			echo "Maybe you need to insert the sdcard, if your device has"
 			echo "any? Trying again in one second..."
 			sleep 1
 		else
-			cryptsetup luksOpen "$partition" root
+			if $(cryptsetup isLuks "$partition"); then
+				cryptsetup luksOpen "$partition" root
+				log "info" "decrypted $partition"
+			else
+				log "info" "unencrypted $partition"
+				break
+			fi
 		fi
 	done
 }
@@ -48,6 +60,7 @@ unlock_root_partition()
 # $1: path to ppm.gz file
 show_splash()
 {
+	log "info" "show_splash $1"
 	gzip -c -d "$1" > /tmp/splash.ppm
 	fbsplash -s /tmp/splash.ppm
 }
