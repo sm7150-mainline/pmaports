ef744e65737e863f75a52f17682ddee14c714420
Clayton Craft <clayton@craftyguy.net>
2017-07-15 09:53:28 -0700

    Account for uInitrd filename change in u-boot script (rx-51)
    The uInitrd filename is appended with 'flavor' so, for example, with the
    rx-51 the generated file is named 'uInitrd-nokia-rx51'. This corrects
    the rx51's u-boot script to account for this change in filename.

--- device-nokia-rx51/uboot-script.cmd
+++ device-nokia-rx51/uboot-script.cmd
@@ -4,7 +4,7 @@ setenv mmctype ext2
 setenv setup_omap_atag 1
 setenv bootargs init=/init.sh rw console=tty0 console=tty02 omapfb_vram=7M omapfb.mode=lcd:848x480-16 nokia-modem.pm=0
 setenv mmckernfile /uImage
-setenv mmcinitrdfile /uInitrd
+setenv mmcinitrdfile /uInitrd-nokia-rx51
 setenv mmcscriptfile
 echo Loading initramfs
 run initrdload
