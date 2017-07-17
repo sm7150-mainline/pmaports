df6247b13e88d7c33b9edcfbc674fa4eff87cf71
Clayton Craft <clayton@craftyguy.net>
2017-07-17 10:13:43 -0700

    [RX51] Export u-boot kernel image with flasher
    This properly exports the uImage kernel, which is used by u-boot, when
    running 'flasher export'. Note the change to the uImage name to follow
    the pattern "uImage-$flavor", which in the case of the RX 51 is
    "uImage-nokia-rx51".

--- device-nokia-rx51/uboot-script.cmd
+++ device-nokia-rx51/uboot-script.cmd
@@ -3,7 +3,7 @@ setenv mmcpart 1
 setenv mmctype ext2
 setenv setup_omap_atag 1
 setenv bootargs init=/init.sh rw console=tty0 console=tty02 omapfb_vram=7M omapfb.mode=lcd:848x480-16 nokia-modem.pm=0
-setenv mmckernfile /uImage
+setenv mmckernfile /uImage-nokia-rx51
 setenv mmcinitrdfile /uInitrd-nokia-rx51
 setenv mmcscriptfile
 echo Loading initramfs
