a69344c067fec567b75be13797974faaf6fca7d2
Oliver Smith <ollieparanoid@bitmessage.ch>
2017-08-09 20:30:41 +0000

    Configfs network setup was broken (related to #338) (#353)
    * Partially resolve #338: Configfs network setup was broken
    * Ignore shellcheck warning
    ...because if we do what shellcheck recommends here, we end up with
    exactly the code which we're reverting in this PR.

--- main/postmarketos-mkinitfs/init_functions.sh
+++ main/postmarketos-mkinitfs/init_functions.sh
@@ -214,7 +214,10 @@ setup_usb_network_configfs() {
 	printf "%s" "rndis" > $CONFIGFS/g1/configs/c.1/strings/0x409/configuration
 
 	ln -s $CONFIGFS/g1/functions/rndis.usb0 $CONFIGFS/g1/configs/c.1
-	ls /sys/class/udc > $CONFIGFS/g1/UDC
+
+	# See also: #338
+	# shellcheck disable=SC2005
+	echo "$(ls /sys/class/udc)" > $CONFIGFS/g1/UDC
 }
 
 setup_usb_network() {
