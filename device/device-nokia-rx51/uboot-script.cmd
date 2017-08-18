0136f832d0546f1e4c6dfefe76ff84141b5d50c5
clayton craft <craftyguy@users.noreply.github.com>
2017-08-18 14:43:31 -0700

    [RX51] Remove unneeded kernel cmdline options (#376)
    Remove omapfb options since they are irrelevant now that we use omapdrm.

--- device/device-nokia-rx51/uboot-script.cmd
+++ device/device-nokia-rx51/uboot-script.cmd
@@ -2,7 +2,7 @@ setenv mmcnum 0
 setenv mmcpart 1
 setenv mmctype ext2
 setenv setup_omap_atag 1
-setenv bootargs init=/init.sh rw console=tty0 console=tty02 omapfb_vram=7M omapfb.mode=lcd:848x480-16 nokia-modem.pm=0
+setenv bootargs init=/init.sh rw console=tty0 console=tty02 nokia-modem.pm=0
 setenv mmckernfile /uImage-postmarketos
 setenv mmcinitrdfile /uInitrd-postmarketos
 setenv mmcscriptfile
