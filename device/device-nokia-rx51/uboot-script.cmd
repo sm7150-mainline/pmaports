4eb52ddff0d0b5769b802659268a094c9789ae16
clayton craft <craftyguy@users.noreply.github.com>
2017-09-13 12:20:46 -0700

    Feature/rx51 ofono (#567)
    * [rx-51] remove pm=0 for nokia-modem
    * Add ofono-1.20 + n900/isimodem fixes
    * [rx51] Enable ofono support

--- device/device-nokia-rx51/uboot-script.cmd
+++ device/device-nokia-rx51/uboot-script.cmd
@@ -2,7 +2,7 @@ setenv mmcnum 0
 setenv mmcpart 1
 setenv mmctype ext2
 setenv setup_omap_atag 1
-setenv bootargs init=/init.sh rw console=tty0 console=tty02 nokia-modem.pm=0
+setenv bootargs init=/init.sh rw console=tty0 console=tty02
 setenv mmckernfile /uImage-postmarketos
 setenv mmcinitrdfile /uInitrd-postmarketos
 setenv mmcscriptfile
