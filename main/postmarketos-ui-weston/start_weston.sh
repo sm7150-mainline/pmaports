a68b9e0e405ab3eb225dd172f3ac8b5d4e13995e
Oliver Smith <ollieparanoid@bitmessage.ch>
2017-10-04 15:28:39 +0000

    Fix #633: start postmarketos-demos with weston (#648)
    * Remove udev trigger/settle, because we start it properly now
    * Try for 10 seconds to start postmarketos-demos, wait 0.5s after each try

--- main/postmarketos-ui-weston/start_weston.sh
+++ main/postmarketos-ui-weston/start_weston.sh
@@ -17,10 +17,16 @@ if test -z "${XDG_RUNTIME_DIR}"; then
             WESTON_OPTS=" --pixman-type=${deviceinfo_weston_pixman_type}"
         fi
 
-		udevadm trigger
-		udevadm settle
-		# Workaround! See #633 for discussing a proper fix
-		(sleep 3; postmarketos-demos) &
+		# #633: Weston doesn't support autostarting applications (yet), so
+		# we try to run postmarketos-demos for 10 seconds, until it succeeds.
+		(
+			for i in $(seq 0 19); do
+				sleep 0.5
+				postmarketos-demos && break
+			done
+		) &
+
+
 		weston ${WESTON_OPTS} >/tmp/weston.log 2>&1
 
 		# In case of failure, restart after 1s
