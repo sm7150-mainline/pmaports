3c5f76f5ddf3a71af413280ef7f179819509958b
clayton craft <craftyguy@users.noreply.github.com>
2017-11-03 16:56:56 -0700

    [rx51] Fix uboot script to use -stable kernel name (#864)

--- device/device-nokia-rx51/uboot-script.cmd
+++ device/device-nokia-rx51/uboot-script.cmd
@@ -3,8 +3,8 @@ setenv mmcpart 1
 setenv mmctype ext2
 setenv setup_omap_atag 1
 setenv bootargs init=/init.sh rw console=tty0 console=tty02
-setenv mmckernfile /uImage-postmarketos
-setenv mmcinitrdfile /uInitrd-postmarketos
+setenv mmckernfile /uImage-postmarketos-stable
+setenv mmcinitrdfile /uInitrd-postmarketos-stable
 setenv mmcscriptfile
 echo Loading initramfs
 run initrdload
