d6efa94dd4ce02b48024427da72f19af69b96146
Bart Ribbers <PureTryOut@users.noreply.github.com>
2017-12-05 00:19:17 +0100

    Re-add powerdevil and polkit-kde-agent as runtime deps to postmarketos-ui-plasma-mobile (#977)
    * Re-add powerdevil and polkit-kde-agent as runtime deps to postmarketos-ui-plasma-mobile
    * Add kwinrc to start applications maximized

--- main/postmarketos-ui-plasma-mobile/start_plasma.sh
+++ main/postmarketos-ui-plasma-mobile/start_plasma.sh
@@ -9,8 +9,7 @@ if test -z "${XDG_RUNTIME_DIR}"; then
 		udevadm trigger
 		udevadm settle
 	
-		console-kit-daemon
-		dbus-launch
+		export QML2_IMPORT_PATH=/usr/lib/qt/qml:/usr/lib/qt5/qml
 
 		sleep 2
 		ck-launch-session kwin_wayland --drm --xwayland -- plasma-phone 2>&1 | logger -t "$(whoami):plasma-mobile"
