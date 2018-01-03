a4dc750a3d77bb092d328a58c037c0a48b2ec671
drebrez <drebrez@gmail.com>
2018-01-03 19:43:13 +0100

    Add initial packaging of MATE Desktop Environment (#1012)
    Notably it has a loading delay (~30s) issue with marco (MATE window manager).

---
+++ main/postmarketos-ui-mate/start_mate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Mate autostart on tty1 (Autologin on tty1 is enabled in
+# /etc/inittab by postmarketos-base post-install.hook).
+# This is a temporary solution, we'll need something like a
+# display manager in the long run (#656).
+if [ "$(id -u)" = "1000" ] && [ "$(tty)" = "/dev/tty1" ]; then
+	# Start X11 with Mate
+	startx /usr/bin/mate-session 2>&1 | logger -t "$(whoami):x11"
+
+
+	# In case of failure, restart after 1s
+	sleep 1
+	exit
+fi
