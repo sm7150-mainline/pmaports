2f52a22248e9000f0304a7fb7f8a3ec2832f3689
Sicelo <sicelo@users.noreply.github.com>
2018-04-23 23:04:31 +0200

    Fix issues with lock.sh for i3wm/N900 (#1439)

--- main/postmarketos-ui-i3wm/lock.sh
+++ main/postmarketos-ui-i3wm/lock.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-FILE=~/tmp/screenoff
+FILE=~/.screenoff
 if [ -f $FILE ]; then
     xinput set-prop 8 "Device Enabled" 1
     xset dpms force on
-    rm ~/.screenoff
+    rm "$FILE"
 else
-    xset dpms force off
     xinput set-prop 8 "Device Enabled" 0
-    touch ~/.screenoff
+    # Turn screen off twice (sometimes it does not work on first run)
+    xset dpms force off
+    xset dpms force off
+    touch "$FILE"
 fi
