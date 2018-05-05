302b6f5c29d093006ba79328712ce7bb6f21453f
Bhushan Shah <bhush94@gmail.com>
2018-05-05 19:21:18 +0530

    Use postmarketOS artwork on plasma-mobile (#1464)

---
+++ main/postmarketos-ui-plasma-mobile/org.kde.phone.homescreen.js
@@ -0,0 +1,5 @@
+applet.wallpaperPlugin = 'org.kde.image'
+applet.currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
+applet.writeConfig("Image", "file:///usr/share/wallpapers/postmarketos.jpg")
+applet.writeConfig("FillMode", "2")
+applet.reloadConfig()
