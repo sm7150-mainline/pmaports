4328e5fe2fbba804b7fad6c9a12312a40ca8ebbe
Oliver Smith <ollieparanoid@bitmessage.ch>
2019-01-29 09:35:10 +0100

    devicepkg-dev: opt-out compiler-gcc.h replacing (!186)
    Instead of never overwriting compiler-gcc.h if it exists, overwrite it
    by default except if OVERWRITE_GCCH=0 is set in the environment.
    
    Most kernels *do* need this file replaced and won't build without this
    patch.

--- main/devicepkg-dev/downstreamkernel_prepare.sh
+++ main/devicepkg-dev/downstreamkernel_prepare.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
+
+# Replace compiler-gcc.h with one that works with newer GCC versions.
+# Set REPLACE_GCCH=0 to avoid replacing an existing compiler-gcc.h file.
+install_gcc_h() {
+	_gcch="$builddir/include/linux/compiler-gcc.h"
+	if [ -f "$_gcch" ]; then
+		if [ "$REPLACE_GCCH" = "0" ]; then
+			echo "NOTE: *not* replacing $_gcch, because of REPLACE_GCCH=0"
+			return
+		else
+			echo "NOTE: replacing $_gcch! If your build breaks with 'Please"
+			echo "don't include <linux/compiler-gcc.h> directly' or a similar"
+			echo "compiler-gcc.h related error, then set"
+			echo "  REPLACE_GCCH=0"
+			echo "in your kernel APKBUILD at the start of the"
+			echo "downstreamkernel_prepare.sh line."
+		fi
+	fi
+
+	cp -v "/usr/share/devicepkg-dev/compiler-gcc.h" "$_gcch"
+}
+
+# Parse arguments
 srcdir=$1
 builddir=$2
 _config=$3
@@ -13,10 +36,8 @@ if [ -z "$srcdir" ] || [ -z "$builddir" ] || [ -z "$_config" ] ||
 	exit 1
 fi
 
-# support newer GCC versions
-if [ ! -f "$builddir/include/linux/compiler-gcc.h" ]; then
-        cp -v "/usr/share/devicepkg-dev/compiler-gcc.h" "$builddir/include/linux/"
-fi
+# Support newer GCC versions
+install_gcc_h
 
 # Remove -Werror from all makefiles
 makefiles="$(find "$builddir" -type f -name Makefile)
