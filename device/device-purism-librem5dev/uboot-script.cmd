2f3b5469b2bf9e2ff1a20d623ca226156b5e6e30
Clayton Craft <clayton@craftyguy.net>
2019-08-31 21:07:54 -0700

    purism-librem5dev: update deviceinfo and uboot script (!590)
    This simplifies the uboot script and updates it to use correct address
    offsets for kernel, ramdisk and fdt. Appending the dtb to the kernel is
    no longer necessary. This also changes the dtb to the one used on
    PureOS.

--- device/device-purism-librem5dev/uboot-script.cmd
+++ device/device-purism-librem5dev/uboot-script.cmd
@@ -1,22 +1,20 @@
-setenv bootargs init=/init.sh rw console=ttymxc0,115200 earlycon=ec_imx7q,0x30860000,115200 video=HDMI-A-1:1920x1080-32@60 usbcore.autosuspend=-1 PMOS_NO_OUTPUT_REDIRECT PMOS_FORCE_PARTITION_RESIZE
+setenv bootargs init=/init.sh rw console=ttymxc0,115200 cma=256M PMOS_NO_OUTPUT_REDIRECT PMOS_FORCE_PARTITION_RESIZE
 setenv mmcdev 0
 setenv mmcpart 1
 
 printenv
 echo Loading DTB
-ext2load mmc ${mmcdev}:${mmcpart} ${fdt_addr} librem5-evk.dtb
+ext2load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} imx8mq-librem5-devkit.dtb
 
 echo Loading Initramfs
-ext2load mmc ${mmcdev}:${mmcpart} ${initrd_addr} uInitrd-purism-librem5dev
+ext2load mmc ${mmcdev}:${mmcpart} ${ramdisk_addr_r} uInitrd-purism-librem5dev
 
 echo Loading Kernel
-ext2load mmc ${mmcdev}:${mmcpart} ${loadaddr} vmlinuz-purism-librem5dev
+ext2load mmc ${mmcdev}:${mmcpart} ${kernel_addr_r} vmlinuz-purism-librem5dev
 
 echo Resizing FDT
-fdt addr ${fdt_addr}
+fdt addr ${fdt_addr_r}
 fdt resize
 
 echo Booting kernel
-booti ${loadaddr} ${initrd_addr} ${fdt_addr}
-
-
+booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}
