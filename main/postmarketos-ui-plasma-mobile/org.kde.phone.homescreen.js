cb822c31609734386dd62db57ca02ef703448ca6
Bart Ribbers <bribbers@disroot.org>
2019-09-30 10:12:51 +0200

    main/postmarketos-ui-plasma-mobile: remove conflicting file with plasma-phone-components (!652)

--- main/postmarketos-ui-plasma-mobile/org.kde.phone.homescreen.js
+++
@@ -1,5 +0,0 @@
-applet.wallpaperPlugin = 'org.kde.image'
-applet.currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
-applet.writeConfig("Image", "file:///usr/share/wallpapers/postmarketos.jpg")
-applet.writeConfig("FillMode", "2")
-applet.reloadConfig()
