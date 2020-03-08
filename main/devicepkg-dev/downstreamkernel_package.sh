a4b1fb8371badbb76608aa92dcb88d6dcdada41c
Dolphin von Chips <DoIphinChips@protonmail.com>
2020-03-08 20:58:05 +0500

    devicepkg-dev: implement out-of-tree build for kernel (!1084)

--- main/devicepkg-dev/downstreamkernel_package.sh
+++ main/devicepkg-dev/downstreamkernel_package.sh
@@ -5,22 +5,23 @@ builddir=$1
 pkgdir=$2
 _carch=$3
 _flavor=$4
+_outdir=$5
 
 if [ -z "$builddir" ] || [ -z "$pkgdir" ] || [ -z "$_carch" ] ||
 	[ -z "$_flavor" ]; then
 	echo "ERROR: missing argument!"
 	echo "Please call downstreamkernel_package() with \$builddir, \$pkgdir,"
-	echo "\$_carch and \$_flavor as arguments."
+	echo "\$_carch, \$_flavor (and optionally \$_outdir) as arguments."
 	exit 1
 fi
 
 # kernel.release
-install -D "$builddir/include/config/kernel.release" \
+install -D "$builddir/$_outdir/include/config/kernel.release" \
 	"$pkgdir/usr/share/kernel/$_flavor/kernel.release"
 
 # zImage (find the right one)
 # shellcheck disable=SC2164
-cd "$builddir/arch/$_carch/boot"
+cd "$builddir/$_outdir/arch/$_carch/boot"
 _target="$pkgdir/boot/vmlinuz-$_flavor"
 
 if [ -n "$KERNEL_IMAGE_NAME" ]; then
--- main/devicepkg-dev/downstreamkernel_prepare.sh
+++ main/devicepkg-dev/downstreamkernel_prepare.sh
@@ -3,6 +3,7 @@
 # Replace compiler-gcc.h with one that works with newer GCC versions.
 # Set REPLACE_GCCH=0 to avoid replacing an existing compiler-gcc.h file.
 install_gcc_h() {
+	# shellcheck disable=SC2154
 	_gcch="$builddir/include/linux/compiler-gcc.h"
 	if [ -f "$_gcch" ]; then
 		if [ "$REPLACE_GCCH" = "0" ]; then
@@ -21,23 +22,19 @@ install_gcc_h() {
 	cp -v "/usr/share/devicepkg-dev/compiler-gcc.h" "$_gcch"
 }
 
-# Parse arguments
-srcdir=$1
-builddir=$2
-_config=$3
-_carch=$4
-HOSTCC=$5
-
-if [ -z "$srcdir" ] || [ -z "$builddir" ] || [ -z "$_config" ] ||
-	[ -z "$_carch" ]; then
-	echo "ERROR: missing argument!"
-	echo "Please call downstreamkernel_prepare() with \$srcdir, \$builddir,"
-	echo "\$_config, \$_carch (and optionally \$HOSTCC) as arguments."
+if [ "$#" -ne 0 ]; then
+	echo "ERROR: downstreamkernel_prepare should be sourced in APKBUILDs."
+	echo "Related: https://postmarketos.org/downstreamkernel-prepare"
 	exit 1
 fi
 
-# Only override HOSTCC if set (to force use of an old gcc)
-[ -z "$HOSTCC" ] || HOSTCC="HOSTCC=$HOSTCC"
+# Set _outdir to "." if not set
+if [ -z "$_outdir" ]; then
+	_outdir="."
+fi
+
+# Set _hostcc when HOSTCC is set
+[ -z "$HOSTCC" ] || _hostcc="HOSTCC=$HOSTCC"
 
 # Support newer GCC versions
 install_gcc_h
@@ -52,6 +49,9 @@ for i in $makefiles; do
 done
 
 # Prepare kernel config ('yes ""' for kernels lacking olddefconfig)
-cp "$srcdir/$_config" "$builddir"/.config
-# shellcheck disable=SC2086
-yes "" | make -C "$builddir" ARCH="$_carch" $HOSTCC oldconfig
+mkdir -p "$builddir/$_outdir"
+# shellcheck disable=SC2154
+cp "$srcdir/$_config" "$builddir"/"$_outdir"/.config
+# shellcheck disable=SC2086,SC2154
+yes "" | make -C "$builddir" ARCH="$_carch" O="$_outdir" \
+	$_hostcc oldconfig
