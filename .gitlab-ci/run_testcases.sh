7c5f01d8921f6690215cadb2c36ceeca650a8e85
Oliver Smith <ollieparanoid@postmarketos.org>
2020-03-14 09:12:57 +0100

    run_testcases.sh: adjust to new device structure (!1066)
    Fix error:
    ERROR: Could not find deviceinfo file for selected device

--- .gitlab-ci/run_testcases.sh
+++ .gitlab-ci/run_testcases.sh
@@ -22,8 +22,8 @@ pmbootstrap -q shutdown
 
 # Make sure we have a valid device (pmbootstrap#1128)
 device="$(pmbootstrap config device)"
-deviceinfo="$pmaports/device/device-$device/deviceinfo"
-if ! [ -e "$deviceinfo" ]; then
+deviceinfo="$pmaports/device/*/device-$device/deviceinfo"
+if ! [ -e $deviceinfo ]; then
 	echo "ERROR: Could not find deviceinfo file for selected device '$device'."
 	echo "Expected path: $deviceinfo"
 	echo "Maybe you have switched to a branch where your device does not exist?"
