d4181b1926c7abc9bc18ed3cc9008c86eb021dd6
Daniele Debernardi <drebrez@gmail.com>
2020-03-17 02:46:43 +0100

    testing/device-nokia-n900: use linux-nokia-n900 (!1073)

--- device/testing/device-nokia-n900/uboot-script.cmd
+++ device/testing/device-nokia-n900/uboot-script.cmd
@@ -3,8 +3,8 @@ setenv mmcpart 1
 setenv mmctype ext2
 setenv setup_omap_atag 1
 setenv bootargs init=/init.sh rw console=tty0 console=tty02 PMOS_NO_OUTPUT_REDIRECT
-setenv mmckernfile /uImage-postmarketos-stable
-setenv mmcinitrdfile /uInitrd-postmarketos-stable
+setenv mmckernfile /uImage-nokia-n900
+setenv mmcinitrdfile /uInitrd-nokia-n900
 setenv mmcscriptfile
 echo Loading initramfs
 run initrdload
