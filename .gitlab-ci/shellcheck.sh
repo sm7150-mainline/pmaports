3e7604601a5c70444955b16020e02148b419b985
Oliver Smith <ollieparanoid@postmarketos.org>
2020-04-11 11:47:41 +0200

    main/postmarketos-mkinitfs: split mkinitfs funcs (!1123)
    Split mkinitfs.sh into two files, so the functions (now in
    mkinitfs_functions.sh) can be sourced in a test. Such a test will be
    added in the next commit.
    
    Move $BINARIES_EXTRA into the get_binaries_extra() function, so its
    $(find...) does not get executed when sourcing the functions file. Move
    $BINARIES into get_binaries() too for consistency.

--- .gitlab-ci/shellcheck.sh
+++ .gitlab-ci/shellcheck.sh
@@ -19,6 +19,7 @@ sh_files="
 	./main/postmarketos-mkinitfs/init.sh.in
 	./main/postmarketos-mkinitfs/init_functions.sh
 	./main/postmarketos-mkinitfs/mkinitfs.sh
+	./main/postmarketos-mkinitfs/mkinitfs_functions.sh
 	./main/postmarketos-mkinitfs-hook-debug-shell/20-debug-shell.sh
 	./main/postmarketos-update-kernel/update-kernel.sh
 	./main/mdss-fb-init-hack/mdss-fb-init-hack.sh
