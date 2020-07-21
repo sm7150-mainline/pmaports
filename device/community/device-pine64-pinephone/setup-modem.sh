15acd86d60fb351e1031c8c47c8a24bbb9c5fee7
Bart Ribbers <bribbers@disroot.org>
2020-07-21 20:04:51 +0200

    pine64-pinephone: enable VoLTE (MR 1434)
    Also fix configuring device sleep wakeup when modem receives a call

--- device/community/device-pine64-pinephone/setup-modem.sh
+++ device/community/device-pine64-pinephone/setup-modem.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+log() {
+	echo "$@" | logger -t "postmarketOS:modem-setup"
+}
+
 # Current modem routing
 #
 #  1 - Digital PCM
@@ -12,17 +16,58 @@
 #  1 - map to first slot (the only slot)
 #
 QDAI_CONFIG="1,0,0,2,0,1,1,1"
-QCFG_CONFIG="physical"
+QCFG_RISIGNALTYPE_CONFIG="physical"
+QMBNCFG_CONFIG="1"
+QCFG_IMS_CONFIG="1"
 
-DEV=/dev/EG25.AT
+if [ -z "$1" ]
+then
+	DEV="/dev/EG25.AT"
+else
+	DEV="$1"
+fi
+
+# When running this script from udev the modem might not be fully initialized
+# yet, so give it some time to initialize
+#
+# We'll try to query for the firmware version for 15 seconds after which we'll
+# consider the initialization failed
+
+log "Waiting for the modem to initialize"
+INITIALIZED=false
+for second in $(seq 1 15)
+do
+        if echo "AT+QDAI?" | atinout - $DEV - | grep -q OK
+        then
+                INITIALIZED=true
+                break
+        fi
+
+        log "Waited for $second seconds..."
+
+        sleep 1
+done
+
+if $INITIALIZED
+then
+        log "Modem initialized"
+else
+        log "Modem failed to initialize"
+        exit 1
+fi
 
 # Read current config
 QDAI_ACTUAL_CONFIG=$(echo "AT+QDAI?" | atinout - $DEV -)
-QCFG_ACTUAL_CONFIG=$(echo 'AT+QCFG="risignaltype"' | atinout - $DEV -)
+QCFG_RISIGNALTYPE_ACTUAL_CONFIG=$(echo 'AT+QCFG="risignaltype"' | atinout - $DEV -)
+QMBNCFG_ACTUAL_CONFIG=$(echo 'AT+QMBNCFG="AutoSel"' | atinout - $DEV -)
+QCFG_IMS_ACTUAL_CONFIG=$(echo 'AT+QCFG="ims"' | atinout - $DEV -)
 
-if echo $QDAI_ACTUAL_CONFIG | grep -q $QDAI_CONFIG && echo $QCFG_ACTUAL_CONFIG | grep -q $QCFG_CONFIG
+if echo $QDAI_ACTUAL_CONFIG | grep -q $QDAI_CONFIG && \
+	echo $QCFG_RISIGNALTYPE_ACTUAL_CONFIG | grep -q $QCFG_RISIGNALTYPE_CONFIG && \
+	echo $QMBNCFG_ACTUAL_CONFIG | grep -q $QMBNCFG_CONFIG && \
+	echo $QCFG_IMS_ACTUAL_CONFIG | grep -q $QCFG_IMS_CONFIG
 then
-	echo "Modem already configured"
+	log "Modem already configured"
 	exit 0
 fi
 
@@ -32,22 +77,37 @@ fi
 # Configure audio
 RET=$(echo "AT+QDAI=$QDAI_CONFIG" | atinout - $DEV -)
 
-if echo $RET | grep -q OK
+if ! echo $RET | grep -q OK
 then
-	echo "Successfully configured modem audio"
-else
-	echo "Failed to set modem audio up: $RET"
+	log "Failed to configure audio: $RET"
 	exit 1
 fi
 
 # Configure ring device
-RET=$(echo 'AT+QCFG="risignaltype","$QCFG_CONFIG"' | atinout - $DEV -)
+RET=$(echo "AT+QCFG=\"risignaltype\",\"$QCFG_RISIGNALTYPE_CONFIG\"" | atinout - $DEV -)
 
-if echo $RET | grep -q OK
+if ! echo $RET | grep -q OK
 then
-	echo "Successfully configured modem ring wakeup"
-else
-	echo "Failed to set modem ring wakeup: $RET"
+	log "Failed to configure modem ring wakeup: $RET"
+	exit 1
+fi
+
+# Configure VoLTE auto selecting profile
+RET=$(echo "AT+QMBNCFG=\"AutoSel\",$QMBNCFG_CONFIG" | atinout - $DEV -)
+
+if ! echo $RET | grep -q OK
+then
+	log "Failed to enable VoLTE profile auto selecting: $RET"
+	exit 1
+fi
+
+# Enable VoLTE
+RET=$(echo "AT+QCFG=\"ims\",$QCFG_IMS_CONFIG" | atinout - $DEV -)
+
+if ! echo $RET | grep -q OK
+then
+	log "Failed to enable VoLTE: $RET"
+	exit 1
 fi
 
 # Reset module
@@ -56,9 +116,8 @@ fi
 #
 RET=$(echo "AT+CFUN=1,1" | atinout - $DEV -)
 
-if echo $RET | grep -q OK
+if ! echo $RET | grep -q OK
 then
-	echo "Successfully reset the modem to apply audio configuration"
-else
-	echo "Failed to reset the modem to apply audio configuration: $RET"
+	log "Failed to reset the module: $RET"
+	exit 1
 fi
