38607b7f12ca43d46714a74b6adefcd772e27d93
Clayton Craft <clayton@craftyguy.net>
2020-09-07 16:32:48 -0700

    device-purism-librem5: select dtb based on device rev (MR 1673)
    [ci:skip-build]: already built successfully in CI

--- device/community/device-purism-librem5/uboot-script.cmd
+++ device/community/device-purism-librem5/uboot-script.cmd
@@ -3,8 +3,17 @@ setenv mmcdev 0
 setenv mmcpart 1
 
 printenv
+
+# select the correct dtb based on device revision
+dtb_file=imx8mq-librem5.dtb
+if itest.s "x2" == "x$board_rev" ; then
+        dtb_file=imx8mq-librem5-r2.dtb
+elif itest.s "x3" == "x$board_rev" ; then
+        dtb_file=imx8mq-librem5-r3.dtb
+fi
+
 echo Loading DTB
-ext2load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} imx8mq-librem5.dtb
+ext2load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${dtb_file}
 
 echo Loading Initramfs
 ext2load mmc ${mmcdev}:${mmcpart} ${ramdisk_addr_r} uInitrd-purism-librem5
