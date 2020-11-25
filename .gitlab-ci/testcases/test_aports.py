88ebc699f3a7444a36412c1d055d3572fae96d4d
Oliver Smith <ollieparanoid@postmarketos.org>
2020-11-25 21:03:10 +0100

    CI: unreferenced files: parse source from checksums (MR 1759)
    postmarketos-base will soon build its source="" variable with a shell
    function. Support this use case by not trying to parse source="" with
    pmbootstrap anymore, but rather parsing the trivial sha512sums
    variable.

--- .gitlab-ci/testcases/test_aports.py
+++ .gitlab-ci/testcases/test_aports.py
@@ -177,12 +177,57 @@ def test_aports_device_kernel(args):
                                path)
 
 
+def parse_source_from_checksums(args, apkbuild_path):
+    """
+    Read the APKBUILD file and parse source files from the checksums at the
+    bottom. This has always the same format, even if $source is built with hard
+    to parse shell code (like in postmarketos-base).
+
+    :param apkbuild_path: full path to the APKBUILD
+    :returns: list of parsed "source" files, e.g.:
+              ["first.patch", "second.patch"]
+    """
+    start = 'sha512sums="'
+    in_block = False
+    ret = []
+
+    with open(apkbuild_path, encoding="utf-8") as handle:
+        for line in handle.readlines():
+            # Find start
+            if not in_block:
+                if line.startswith(start):
+                    in_block = True
+                else:
+                    continue
+
+            # sha512sums may have lines without checksums:
+            # https://gitlab.alpinelinux.org/alpine/abuild/-/merge_requests/73
+            if "  " not in line:
+                continue
+
+            try:
+                _, filename = line.rstrip().split("  ", 2)
+            except ValueError:
+                raise ValueError("Failed to parse checksums. Try to delete the"
+                                 " checksums and generate them again with"
+                                 f" 'pmbootstrap checksum': {apkbuild_path}")
+
+            # Find end
+            if filename.endswith('"'):
+                ret += [filename[:-1]]
+                break
+
+            ret += [filename]
+    return ret
+
+
 def test_aports_unreferenced_files(args):
     """
     Raise an error if an unreferenced file is found
     """
     for apkbuild_path in glob.iglob(args.aports + "/**/APKBUILD", recursive=True):
         apkbuild = pmb.parse.apkbuild(args, apkbuild_path)
+        sources_chk = parse_source_from_checksums(args, apkbuild_path)
 
         # Collect install files from subpackages
         subpackage_installs = []
@@ -208,7 +253,7 @@ def test_aports_unreferenced_files(args):
             if rel_file_path == "APKBUILD" or os.path.isdir(file):
                 continue
 
-            if rel_file_path not in apkbuild["source"] \
+            if os.path.basename(rel_file_path) not in sources_chk \
                     and rel_file_path not in apkbuild["install"] \
                     and rel_file_path not in subpackage_installs \
                     and rel_file_path not in trigger_sources:
