4d327313e40b48bdb9ac0c6e07f0b36cc2be4ff8
Henrik Grimler <henrik@grimler.se>
2020-12-16 08:23:44 +0100

    device-samsung-dream: use minimal initfs-hook (MR 1843)
    Instead of running the entire setup_usb_network_configfs function we
    run the parts that are actually necessary for
    setup_usb_network_android to succeed when it is run afterwards.

--- device/testing/device-samsung-dream/initfs-hook.sh
+++ device/testing/device-samsung-dream/initfs-hook.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-# shellcheck disable=SC1091
-. /etc/deviceinfo
-. ./init_functions.sh
 
-# Device requires usb network initialization through configfs first
-setup_usb_network_configfs
+# This is a trimmed down version of setup_usb_network_configfs from
+# pmOS's init_functions.sh
+
+# See: https://www.kernel.org/doc/Documentation/usb/gadget_configfs.txt
+CONFIGFS=/config/usb_gadget
+
+# Create an usb gadet configuration
+mkdir $CONFIGFS/g1 || echo "initfs-hook: Couldn't create $CONFIGFS/g1"
+
+# Create rndis function.
+mkdir $CONFIGFS/g1/functions/rndis.usb0 \
+	|| echo "initfs-hook: Couldn't create $CONFIGFS/g1/functions/rndis.usb0"
+
+# Create configuration instance for the gadget
+mkdir $CONFIGFS/g1/configs/c.1 \
+	|| echo "initfs-hook: Couldn't create $CONFIGFS/g1/configs/c.1"
+
+# Link the rndis instance to the configuration
+ln -s $CONFIGFS/g1/functions/rndis.usb0 $CONFIGFS/g1/configs/c.1 \
+	|| echo "initfs-hook: Couldn't symlink rndis.usb0"
