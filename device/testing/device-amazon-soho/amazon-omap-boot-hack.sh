08bdfccec7db542665f9c23d6ee99c3ccbe19839
kekzoz <J__A__K@hotmail.com>
2021-10-13 21:15:56 +0100

    amazon-soho: Fix postprocess script (MR 2602)
    [ci:skip-build] already built successfully in CI

--- device/testing/device-amazon-soho/amazon-omap-boot-hack.sh
+++ device/testing/device-amazon-soho/amazon-omap-boot-hack.sh
@@ -5,7 +5,7 @@ SOHO_HEADER_DATA='\x50\x03\x00\x00\x00\x25\xe4\x00'
 SOHO_HEADER_SIZE=848
 SOHO_HEADER_OFFSET=52
 tempfile=$(mktemp)
-bootimgfile=$(echo "${1}" | sed 's/initramfs-/boot.img-/g')
+bootimgfile=$(echo "${1}" | sed 's/initramfs/boot.img/g')
 dd if=/dev/zero of="${tempfile}" bs=$SOHO_HEADER_SIZE count=1
 printf "%b" $SOHO_HEADER_DATA | dd of="${tempfile}" bs=$SOHO_HEADER_OFFSET seek=1 conv=notrunc
 cat "${bootimgfile}" >> "${tempfile}"
