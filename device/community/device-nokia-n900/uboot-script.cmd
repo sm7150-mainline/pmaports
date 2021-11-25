7ffeea5cb7d6436d7ca5d80ad06bafba3bbb35fc
Danct12 <danct12@disroot.org>
2021-11-25 21:39:00 +0700

    device-nokia-n900: fix u-boot script typo (MR 2708)

--- device/community/device-nokia-n900/uboot-script.cmd
+++ device/community/device-nokia-n900/uboot-script.cmd
@@ -3,8 +3,8 @@ setenv mmcpart 1
 setenv mmctype ext2
 setenv setup_omap_atag 1
 setenv bootargs init=/init.sh rw console=tty0 console=ttyO2 PMOS_NO_OUTPUT_REDIRECT PMOS_FORCE_PARTITION_RESIZE
-setenv mmckernfile /uImage-nokia-n900
-setenv mmcinitrdfile /uInitrd-nokia-n900
+setenv mmckernfile /uImage
+setenv mmcinitrdfile /uInitrd
 setenv mmcscriptfile
 echo Loading initramfs
 run initrdload
