26c2085d81345f584bd3fea48a036a0f8e9ac3d3
Johannes Marbach <n0-0ne+gitlab@mailbox.org>
2022-01-22 19:15:41 +0100

    main/postmarketos-mkinitfs: support unl0kr (MR 2687)
    [ci:skip-build] already built successfully in CI

--- main/postmarketos-mkinitfs/init_functions.sh
+++ main/postmarketos-mkinitfs/init_functions.sh
@@ -62,6 +62,13 @@ setup_firmware_path() {
 setup_mdev() {
 	echo /sbin/mdev >/proc/sys/kernel/hotplug
 	mdev -s
+
+	if command -v udevadm > /dev/null; then
+		# Initialise input devices for libinput / unl0kr
+		for i in /dev/input/event*; do
+			udevadm test "/sys/class/input/${i##/dev/input/}"
+		done
+	fi
 }
 
 get_uptime_seconds() {
@@ -296,8 +303,10 @@ resize_root_partition() {
 unlock_root_partition() {
 	partition="$(find_root_partition)"
 	if cryptsetup isLuks "$partition"; then
+		tried=0
 		until cryptsetup status root | grep -qwi active; do
-			start_onscreen_keyboard
+			fde-unlock "$partition" "$tried"
+			tried=$((tried + 1))
 		done
 		ROOT_PARTITION_UNLOCKED=1
 		# Show again the loading splashscreen
@@ -491,58 +500,6 @@ start_unudhcpd() {
 	(
 		unudhcpd -i "$INTERFACE" -s 172.16.42.1 -c 172.16.42.2
 	) &
-}
-
-# $1: SDL_VIDEODRIVER value (e.g. 'kmsdrm', 'directfb')
-run_osk_sdl() {
-	unset ETNA_MESA_DEBUG
-	unset SDL_VIDEODRIVER
-	unset DFBARGS
-	unset TSLIB_TSDEVICE
-	unset OSK_EXTRA_ARGS
-	case "$1" in
-		"kmsdrm")
-			# Set up SDL and Mesa env to use kmsdrm backend
-			export SDL_VIDEODRIVER="kmsdrm"
-			# needed for librem 5
-			export ETNA_MESA_DEBUG="no_supertile"
-			;;
-		"directfb")
-			# Set up directfb and tslib
-			# Note: linux_input module is disabled since it will try to take over
-			# the touchscreen device from tslib (e.g. on the N900)
-			# Note: ps2mouse module is disabled because it causes
-			# jerky/inconsistent touch input on some devices
-			export DFBARGS="system=fbdev,no-cursor,disable-module=linux_input,disable-module=ps2mouse"
-			export SDL_VIDEODRIVER="directfb"
-			# SDL/directfb tries to use gles even though it's not
-			# actually available, so disable it in osk-sdl
-			export OSK_EXTRA_ARGS="--no-gles"
-			# shellcheck disable=SC2154
-			if [ -n "$deviceinfo_dev_touchscreen" ]; then
-				export TSLIB_TSDEVICE="$deviceinfo_dev_touchscreen"
-			fi
-			;;
-	esac
-
-	# osk-sdl needs evdev for input and doesn't launch without it, so
-	# make sure the module isn't missed
-	modprobe evdev
-	osk-sdl $OSK_EXTRA_ARGS -n root -d "$partition" -c /etc/osk.conf \
-		-o /boot/osk.conf -v > /osk-sdl.log 2>&1
-}
-
-start_onscreen_keyboard() {
-	# shellcheck disable=SC2154
-	if [ -n "$deviceinfo_mesa_driver" ]; then
-		# try to run osk-sdl with kmsdrm driver, then fallback to
-		# directfb if that fails
-		if ! run_osk_sdl "kmsdrm"; then
-			run_osk_sdl "directfb"
-		fi
-	else
-		run_osk_sdl "directfb"
-	fi
 }
 
 start_charging_mode() {
