b53b455af8e9328da3ca9355023a8bf68a080318
Adam Plumb <adamplumb@gmail.com>
2022-02-01 12:55:28 -0500

    main/postmarketos-mkinitfs-hook-netboot: Use deviceinfo to determine rootfs sector size if set (MR 2898)

--- main/postmarketos-mkinitfs-hook-netboot/netboot.sh
+++ main/postmarketos-mkinitfs-hook-netboot/netboot.sh
@@ -4,6 +4,7 @@
 . ./init_functions.sh
 NBD_PORT=9999
 NBD_IP=172.16.42.2
+NBD_BLOCK_SIZE=${deviceinfo_rootfs_image_sector_size:-512}
 
 setup_usb_network
 start_unudhcpd
@@ -20,7 +21,7 @@ if [ ! -b /dev/nbd0 ]; then
 	pmos_loop_forever
 fi
 
-while ! busybox nbd-client $NBD_IP $NBD_PORT /dev/nbd0 -b 512; do
+while ! busybox nbd-client $NBD_IP $NBD_PORT /dev/nbd0 -b "$NBD_BLOCK_SIZE"; do
 	echo "Connection attempt not successful, continuing..."
 	sleep 1
 done
