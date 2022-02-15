c67ae2a4de949e0d6e34ee0bf93796cd7b0a3432
Jakob Hauser <jahau@rocketmail.com>
2022-02-15 22:47:57 +0100

    main/postmarketos-ui-xfce4: upgrade to 0.5.1 (MR 2935)
    The main change is to move the postmarketOS-specific Xfce4 configs
    from /etc/skel to /etc/xdg. This improves the config handling because
    the location in /etc/xdg can be defined as the default fallback
    configs.
    
    Moving the gtk-3.0 folder to /etc/xdg did not work. Therefore this
    one remains in /etc/skel.
    
    Beyond this, there are some minor changes from the xfce4-phone
    repository:
    - Change icon path of whiskermenu button to new location
    - Apply pmOS wallpaper to more monitors
    - Several clean-ups in the config files

---
+++ main/postmarketos-ui-xfce4/xfce4-phone.sh
@@ -0,0 +1,4 @@
+# shellcheck shell=sh
+# Set primary config directory for postmarketOS Xfce4 configs.
+# The general directory /etc/xdg will be appended later on.
+export XDG_CONFIG_DIRS="/etc/xdg/xfce4-phone"
