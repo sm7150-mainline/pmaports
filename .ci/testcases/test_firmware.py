b75e662f90e9666ee9ca23726e16637e6cf1c93a
Joel Selvaraj <jo@jsfamily.in>
2022-03-22 15:59:33 +0530

    CI: don't exclude sdm845 related firmware packages (MR 2982)
    No longer needed as these firmware packages doesn't depend on
    soc-qcom-sdm845-nonfree-firmware anymore.

--- .ci/testcases/test_firmware.py
+++ .ci/testcases/test_firmware.py
@@ -17,12 +17,9 @@ def test_aports_firmware(args):
     excluded = [
         "firmware-motorola-potter",  # Depends on soc-qcom-msm8916-ucm
         "firmware-oneplus-msm8998",  # Depends on soc-qcom-sdm845-nonfree-firmware
-        "firmware-oneplus-sdm845",  # Depends on soc-qcom-sdm845-nonfree-firmware
         "firmware-samsung-baffinlite",  # Depends on firmware-aosp-broadcom-wlan
         "firmware-samsung-crespo",  # Depends on firmware-aosp-broadcom-wlan
         "firmware-samsung-maguro",  # Depends on firmware-aosp-broadcom-wlan
-        "firmware-shift-sdm845",  # Depends on soc-qcom-sdm845-nonfree-firmware
-        "firmware-xiaomi-beryllium",  # Depends on soc-qcom-sdm845-nonfree-firmware
         "firmware-xiaomi-ferrari",  # Depends on soc-qcom-msm8916
         "firmware-xiaomi-willow",  # Doesn't build, source link is dead (pma#1212)
     ]
