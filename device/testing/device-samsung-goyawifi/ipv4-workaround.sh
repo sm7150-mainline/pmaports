eb0896b1700808c466bc21019170fb213025862a
cheadrian <che.adrian@yahoo.com>
2022-06-27 16:14:13 -0400

    samsung-goyawifi: new device (MR 3258)
    Bring up support for Samsung T110 Goyawifi.
    Working: Display (framebuffer), touchscreen, buttons and Wi-Fi.
    Include: kernel source and necesarry firmware for Wi-Fi and Bluetooth.
    
    samsung-goyawifi: fix patch names
    
    samsung-goyawifi: remove mesa-dri-gallium
    
    samsung-goyawifi: fix keyboard in deviceinfo
    
    samsung-goyawifi: fix lightdm
    
    samsung-goyawifi: editor-config ci tabs
    
    samsung-goyawifi: add linux kernel as depends
    
    [ci:skip-build]: Already built successfully in CI

---
+++ device/testing/device-samsung-goyawifi/ipv4-workaround.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# It's seems that the networkmanager can't properly set the interface IP
+# and routes on this device. Setting them using iproute2 or net-tools
+# makes the interface to work with network, using static or DHCP ipv4.
+
+IF=$1
+STATUS=$2
+
+if [ "$IF" == "mlan0" ]; then
+	if [ "$STATUS" == "up" ]; then
+		logger -s "Setting IP on $IF"
+		logger -s "IP4_ADDRESS_0 $IP4_ADDRESS_0"
+		logger -s "IP4_GATEWAY $IP4_GATEWAY"
+		logger -s "IP4_ROUTE_0 $IP4_ROUTE_0"
+		ip address add ${IP4_ADDRESS_0% *} brd + dev $IF
+		ip route add default via ${IP4_ADDRESS_0#* } dev $IF
+	fi
+fi
