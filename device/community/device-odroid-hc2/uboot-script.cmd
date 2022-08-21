7d72743b1fcd0d5febac4a049a517f22f2affd0b
Dylan Van Assche <me@dylanvanassche.be>
2022-08-21 14:01:56 +0200

    device-odroid-hc2: switch to linux-edge (MR 3378)
    ODROID HC2 is based upon the Samsung Exynos 5422 and can run full
    mainline. Switch to linux-edge from Alpine to enable this and drop
    the close-to-mainline fork for this device. The close-to-mainline
    fork was already linux-edge for a while but with a separate KConfig.
    These KConfig changes are now integrated in linux-edge of Alpine.
    
    U-boot script was updated to handle vmlinuz-edge as well since
    Alpine's linux-edge does not generate a vmlinuz file, but vmlinuz-edge.
    Probably to allow installing linux-lts as well.
    
    initcall_blacklist=exynos_drm_init was added to the kernel cmdline
    because upstream wants to set CONFIG_DRM_EXYNOS=y instead of module.
    The DRM subsystem of Exynos5 in the kernel seems to be broken for this
    device. There's also no use for it since this device does not have video
    out. See https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/38094

--- device/community/device-odroid-hc2/uboot-script.cmd
+++ device/community/device-odroid-hc2/uboot-script.cmd
@@ -8,10 +8,17 @@ setenv dtb_file "exynos5422-odroidhc1.dtb"
 printenv
 
 echo Setting bootargs
-setenv bootargs init=/init.sh rw console=tty0 console=ttySAC2,115200 panic=10 consoleblank=0 loglevel=9 cma=256M PMOS_NO_OUTPUT_REDIRECT PMOS_FORCE_PARTITION_RESIZE
+setenv bootargs init=/init.sh rw console=tty0 console=ttySAC2,115200 panic=10 consoleblank=0 loglevel=9 cma=256M initcall_blacklist=exynos_drm_init PMOS_NO_OUTPUT_REDIRECT PMOS_FORCE_PARTITION_RESIZE
 
-echo Loading Kernel
-load mmc ${mmcbootdev}:${mmcbootpart} ${kernel_addr_r} ${kernel_image}
+if test -e mmc ${mmcbootdev}:${mmcbootpart} ${kernel_image}; then
+    echo Loading Kernel: vmlinuz
+    load mmc ${mmcbootdev}:${mmcbootpart} ${kernel_addr_r} ${kernel_image};
+fi
+
+if test -e mmc ${mmcbootdev}:${mmcbootpart} ${kernel_image}-edge; then
+    echo Loading Kernel: vmlinuz-edge
+    load mmc ${mmcbootdev}:${mmcbootpart} ${kernel_addr_r} ${kernel_image}-edge
+fi
 
 echo Loading Initramfs
 load mmc ${mmcbootdev}:${mmcbootpart} ${initrd_addr_r} ${initrd_image}
