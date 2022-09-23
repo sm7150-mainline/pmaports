8d07a9897ac5077f99b9a3e47f4d08502fb6ef66
Sergey Lisov <sleirsgoevy@gmail.com>
2022-09-23 10:17:56 +0300

    mdoem/libsamsung-ipc: upgrade to 6.0.0004 (MR 3456)
    1. Upgrades the library to version 6.0.0004
    2. Adds runtime overrides for detected device and board,
       to facilitate running on devices where autodetect fails.

--- modem/samsungipcd/samsungipcd.sh
+++ modem/samsungipcd/samsungipcd.sh
@@ -3,6 +3,16 @@
 description="AT modem emulation using libsamsung-ipc"
 depends="ppp"
 
-command="samsungipcd"
-command_args="/dev/ptywc /dev/ptywd"
+command="env"
+command_args="samsungipcd /dev/ptywc /dev/ptywd"
 supervisor="supervise-daemon"
+
+if [[ -n "$IPC_DEVICE_NAME" ]]
+then
+	command_args="IPC_DEVICE_NAME=$IPC_DEVICE_NAME $command_args"
+fi
+
+if [[ -n "$IPC_DEVICE_BOARD_NAME" ]]
+then
+	command_args="IPC_DEVICE_BOARD_NAME=$IPC_DEVICE_BOARD_NAME $command_args"
+fi
