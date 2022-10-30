196abb1575d303d60000af417bba652f5704433e
Oliver Smith <ollieparanoid@postmarketos.org>
2022-10-30 17:34:06 +0100

    CI: kconfig adjust for 'pmbootstrap ci' (MR 3608)

---
+++ .ci/kconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+# Description: check all kernel configs with 'pmbootstrap kconfig check'
+# Options: native
+# Use 'native' because it requires running pmbootstrap.
+# https://postmarktos.org/pmb-ci
+
+if [ "$(id -u)" = 0 ]; then
+	set -x
+	wget "https://gitlab.com/postmarketOS/ci-common/-/raw/master/install_pmbootstrap.sh"
+	sh ./install_pmbootstrap.sh
+	exec su "${TESTUSER:-pmos}" -c "sh -e $0"
+fi
+
+# Wrap pmbootstrap to use this repository for --aports
+pmaports="$(cd $(dirname $0)/..; pwd -P)"
+_pmbootstrap="$(command -v pmbootstrap)"
+pmbootstrap() {
+	"$_pmbootstrap" --aports="$pmaports" "$@"
+}
+
+set -x
+pmbootstrap kconfig check
