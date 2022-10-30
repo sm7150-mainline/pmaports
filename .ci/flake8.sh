613aa1c68a3dc4b2174a489d4466d337f25bd18a
Oliver Smith <ollieparanoid@postmarketos.org>
2022-10-30 15:21:20 +0100

    CI: flake8: adjust for 'pmbootstrap ci' (MR 3608)

---
+++ .ci/flake8.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+# Description: lint CI related python scripts/tests
+# https://postmarktos.org/pmb-ci
+
+if [ "$(id -u)" = 0 ]; then
+	set -x
+	apk -q add py3-flake8
+	exec su "${TESTUSER:-build}" -c "sh -e $0"
+fi
+
+set -x
+
+# shellcheck disable=SC2046
+flake8 --ignore E501,F401,E722,W504,W605 $(find .ci -name '*.py')
