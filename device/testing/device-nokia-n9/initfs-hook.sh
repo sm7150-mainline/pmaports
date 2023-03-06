6873b6380393659f33af4d7b725e888c8490fe14
Clayton Craft <clayton@craftyguy.net>
2023-03-05 23:24:55 -0800

    device-nokia-n9: support new mkintifs paths (MR 3895)

--- device/testing/device-nokia-n9/initfs-hook.sh
+++ device/testing/device-nokia-n9/initfs-hook.sh
@@ -6,7 +6,7 @@ watchdog_kick() {
 			[ -c $wd ] && echo X > $wd
 		done
 
-		# /etc/postmarketos-mkinitfs/hooks/00-device-nokia-rm696.sh: line 12: sleep: not found
+		# /usr/share/mkinitfs/hooks/00-device-nokia-rm696.sh: line 12: sleep: not found
 		if [ -f /bin/sleep ]; then
 			/bin/sleep 2s
 		else
