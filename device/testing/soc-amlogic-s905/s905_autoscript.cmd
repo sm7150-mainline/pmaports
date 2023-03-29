287e7c4521f613eefc19a39b83556fbdab1ad352
@exkc <exkc@exkc.moe>
2023-03-29 23:29:45 +0800

    soc-amlogic-s905: enable ext2/ext4 support for the boot partition (MR 3955)

--- device/testing/soc-amlogic-s905/s905_autoscript.cmd
+++ device/testing/soc-amlogic-s905/s905_autoscript.cmd
@@ -1,2 +1,4 @@
 if fatload mmc 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
 if fatload usb 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
+if ext4load mmc 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
+if ext4load usb 0 0x1000000 u-boot.bin; then go 0x1000000; fi;
