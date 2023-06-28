73e05bf16dd0bcd2e1b1270656dc7c1ec41a42b6
Caleb Connolly <caleb@connolly.tech>
2023-06-28 23:54:26 +0100

    main/postmarketos-initramfs: silence pbsplash (MR 4204)

--- main/postmarketos-initramfs/init_functions.sh
+++ main/postmarketos-initramfs/init_functions.sh
@@ -630,7 +630,7 @@ show_splash() {
 	# shellcheck disable=SC2154,SC2059
 	/usr/bin/pbsplash -s /usr/share/pbsplash/pmos-logo-text.svg \
 		-b "Linux $(uname -r) | $deviceinfo_codename" \
-		-m "$(printf "$1")" &
+		-m "$(printf "$1")" >/dev/null &
 }
 
 hide_splash() {
