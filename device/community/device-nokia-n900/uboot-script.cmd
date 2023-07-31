73a033328e3399f1d217e3b3d20f9a547dc0b939
Sicelo A. Mhlongo <absicsz@gmail.com>
2023-07-31 22:26:21 +0200

    device-nokia-n900: set uniprocessor mode for kernel (MR 4299)
    The OMAP 3430 in the Nokia N900 is a uniprocessor SoC, so disable SMP via
    kernel command line. This appears to alleviate the ongoing kernel crashes
    affecting the Nokia N900 in pmOS.
    
    [ci:skip-build]: already built successfully in CI

--- device/community/device-nokia-n900/uboot-script.cmd
+++ device/community/device-nokia-n900/uboot-script.cmd
@@ -2,7 +2,7 @@ setenv mmcnum 0
 setenv mmcpart 1
 setenv mmctype ext2
 setenv setup_omap_atag 1
-setenv bootargs init=/init.sh rw console=tty0 console=ttyS2 PMOS_NO_OUTPUT_REDIRECT PMOS_FORCE_PARTITION_RESIZE
+setenv bootargs init=/init.sh rw console=tty0 console=ttyS2 maxcpus=0 PMOS_NO_OUTPUT_REDIRECT PMOS_FORCE_PARTITION_RESIZE
 setenv mmckernfile /uImage
 setenv mmcinitrdfile /uInitrd
 setenv mmcscriptfile
