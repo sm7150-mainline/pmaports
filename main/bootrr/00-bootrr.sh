d67268bef9db9ac503ff4fcca6453f126a6c186d
Caleb Connolly <caleb@connolly.tech>
2023-11-29 00:40:18 +0000

    main/bootrr: detect failure (MR 4542)
    Properly detect failures in bootrr
    
    Signed-off-by: Caleb Connolly <caleb@connolly.tech>

--- main/bootrr/00-bootrr.sh
+++ main/bootrr/00-bootrr.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -e
 # Description: bootrr sanity check
 
-bootrr
+bootrr | tee bootrr.log
+
+if [ grep -q 'fail' bootrr.log ]; then
+	echo "PMOS-CI-TEST: bootrr sanity check failed"
+	exit 1
+fi
