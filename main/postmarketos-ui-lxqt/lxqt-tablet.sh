753b12a793573618f5ad7d4b1f84b0b66d05443d
Jakob Hauser <jahau@rocketmail.com>
2023-11-30 22:34:52 +0100

    main/postmarketos-ui-lxqt: new set of improvements (MR 4623)
    Dependencies:
    - Add dependency "xfce4-screensaver" to provide virtual keyboard at screen
      unlock. As a dependency because several settings need to be implemented to
      integrate it well in LXQt.
    - Add dependency "setxkbmap" to make keyboard layout settings work
    - Add dependency "upower" to make battery icon in panel work
    
    Recommends:
    - Add recommend "pulseaudio" to make sound work
    - Add recommend "adwaita-icon-theme" because that's used by xfce4-screensaver
    - Remove recommend "pavucontrol" because there is already "pavucontrol-qt"
    
    Sources:
    - Add autostart desktop file for xfce4-screensaver
    - Add autostart desktop file to override lxqt-xscreensaver-autostart
    - Modify autostart desktop file for onboard, a startup delay mitigates
      an issue with the onboard panel icon not showing up at the first boot
    - Modify lxqt-tablet.sh to include custom XDG_DATA_DIRS
    - Add application desktop file to override xfce4-screensaver-preferences,
      adding it to the menu
    - Add application desktop file to override xscreensaver-settings, removing it
      from the menu
    - Change install location of "lxqt-tablet" repository configs from
      /etc/skel/.config to /etc/xdg/lxqt-tablet. That way they are handled as
      distribution-specific configs and are separated from the user configs in
      ~/.config.
    - Upgrade to version 0.3 of "lxqt-tablet" repository:
      - Change theme to "Dark" to fix panel look
      - Increase panel size
      - Change wallpaper mode to "zoom" to avoid deforming the picture
      - Disable global single click to not interfere with PCManFM
      - Add terminal and file manager launchers to panel
      - Rename openbox xml file to apply the configs
      - Set lock command to xfce4-screensaver
      - Add xfconf files to configure xfce4-screensaver
      - Clean up configs by removing default values
    
    [ci:skip-build]: Already built successfully in CI.

--- main/postmarketos-ui-lxqt/lxqt-tablet.sh
+++ main/postmarketos-ui-lxqt/lxqt-tablet.sh
@@ -1,3 +1,4 @@
 # shellcheck shell=sh
-# Set primary config directory for postmarketOS LXQt configs.
+# Set primary directories for postmarketOS LXQt configs and data.
 export XDG_CONFIG_DIRS="/etc/xdg/lxqt-tablet:/etc/xdg:/etc:/usr/share"
+export XDG_DATA_DIRS="/usr/share/lxqt-tablet:/usr/local/share:/usr/share"
