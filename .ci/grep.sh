b5b7983ade2c23f9a125a9d83ac955eb932c59cf
Clayton Craft <clayton@craftyguy.net>
2024-01-18 13:49:27 -0800

    ci/grep.sh: always run all tests (MR 4740)
    fixes #2543

--- .ci/grep.sh
+++ .ci/grep.sh
@@ -2,6 +2,8 @@
 # Description: check various bad patterns with grep
 # https://postmarketos.org/pmb-ci
 
+exit_code=0
+
 if [ "$(id -u)" = 0 ]; then
 	set -x
 	apk -q add grep
@@ -12,7 +14,7 @@ fi
 if grep -qr '(CHANGEME!)' -- *; then
 	echo "ERROR: Please replace '(CHANGEME!)' in the following files:"
 	grep --color=always -r '(CHANGEME!)' -- *
-	exit 1
+	exit_code=1
 fi
 
 # DTBs installed to /usr/share/db
@@ -23,7 +25,7 @@ if grep -qr 'INSTALL_DTBS_PATH="$pkgdir"/usr/share/dtb' device/; then
 	echo 'ERROR: Files that need fixing:'
 	# shellcheck disable=SC2016
 	grep --color=always -r 'INSTALL_DTBS_PATH="$pkgdir"/usr/share/dtb' device/
-	exit 1
+	exit_code=1
 fi
 
 
@@ -31,26 +33,26 @@ fi
 if grep -qr '/etc/postmarketos-mkinitfs' -- *; then
 	echo "ERROR: Please replace '/etc/postmarketos-mkinitfs' with '/usr/share/mkinitfs' in the following files:"
 	grep --color=always -r '/etc/postmarketos-mkinitfs' -- *
-	exit 1
+	exit_code=1
 fi
 if grep -qr '/usr/share/postmarketos-mkinitfs' -- *; then
 	echo "ERROR: Please replace '/usr/share/postmarketos-mkinitfs' with '/usr/share/mkinitfs' in the following files:"
 	grep --color=always -r '/usr/share/postmarketos-mkinitfs' -- *
-	exit 1
+	exit_code=1
 fi
 
 # Direct sourcing of deviceinfo
 if grep --exclude='source_deviceinfo' -qEr 'source /etc/deviceinfo|\. /etc/deviceinfo' -- *; then
 	echo 'ERROR: Please source the source_deviceinfo script instead of sourcing deviceinfo directly!'
 	grep --color=always --exclude='rootfs-usr-share-misc-source_deviceinfo' -Er 'source /etc/deviceinfo|\. /etc/deviceinfo' -- *
-	exit 1
+	exit_code=1
 fi
 
 # Removed deviceinfo variable
 if grep -qr 'deviceinfo_modules_initfs' -- *; then
 	echo 'ERROR: deviceinfo_modules_initfs variable has been removed. Use "modules-initfs" file instead.'
 	grep --color=always -r 'deviceinfo_modules_initfs' -- *
-	exit 1
+	exit_code=1
 fi
 
 POSTMARKETOS_WALLPAPER_PATH='/usr/share/wallpapers/postmarketos.jpg'
@@ -69,5 +71,7 @@ if grep -qr $POSTMARKETOS_WALLPAPER_PATH \
 		--exclude-dir='device-xiaomi-willow' \
 		--exclude-dir='device-nokia-n900' \
 		-- device
-	exit 1
+	exit_code=1
 fi
+
+exit "$exit_code"
