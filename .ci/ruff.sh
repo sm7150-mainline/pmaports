73b1a493b94d78dec2c0568a399ffcf6b66915cd
Stefan Hansson <newbyte@postmarketos.org>
2024-01-18 22:52:17 +0100

    ci: replace Flake8 with Ruff (MR 4741)
    See similar patch in pmbootstrap for rationale:
    https://gitlab.com/postmarketOS/pmbootstrap/-/commit/27618d5ffdb0b6cbf9471dc01a2ed14e1655ced0
    
    W504 ignore is removed as the rule does not exist in Ruff. But, since it
    was ignored anyway, this does not matter.

--- .ci/ruff.sh
+++ .ci/ruff.sh
@@ -4,11 +4,11 @@
 
 if [ "$(id -u)" = 0 ]; then
 	set -x
-	apk -q add py3-flake8
+	apk -q add ruff
 	exec su "${TESTUSER:-build}" -c "sh -e $0"
 fi
 
 set -x
 
 # shellcheck disable=SC2046
-flake8 --ignore E501,F401,E722,W504,W605 $(find .ci -name '*.py')
+ruff --ignore E501,F401,E722,W605 $(find .ci -name '*.py')
